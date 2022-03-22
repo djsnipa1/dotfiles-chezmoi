98985ba86ccbf9bfbbcf34e505acc75e10425794
Chad Boyce <chadboyce@gmail.com>
2022-03-21 22:14:56 -0400

    added stuff

--- private_dot_config/nvim/lua/user/plugins.lua
+++ private_dot_config/nvim/lua/user/plugins.lua
@@ -116,6 +116,9 @@ return packer.startup(function(use)
 
   use "Zabanaa/neuromancer.vim"
 
+  -- for chezmoi dotfiles
+  use { "alker0/chezmoi.vim" }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
